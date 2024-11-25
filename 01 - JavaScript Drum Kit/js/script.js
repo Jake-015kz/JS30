b0f6577e5f848e94f978d233b879dff6e934a9a7
user.name "zhegan89@gmail.com <zhegan89@gmail.com>
2024-11-26 02:18:53 +0500

    Refactored files and initiated project 03: JavaScript and CSS Variables. Created necessary structures.

--- 01 - JavaScript Drum Kit/js/script.js
+++ 01 - JavaScript Drum Kit/js/script.js
@@ -1,30 +1,31 @@
-const  keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll(".key");
 
 function keyClicked() {
-    const keyCode = this.dataset.key;
-    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-    
-    if(audio) {
-        audio.currentTime = 0;
-        audio.play();
+  const keyCode = this.dataset.key;
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 
-        this.classList.add('playing');
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
 
-        this.addEventListener('transitionend', () => {
-            this.classList.remove('playing');
-        },{once: true}
+    this.classList.add("playing");
+
+    this.addEventListener(
+      "transitionend",
+      () => {
+        this.classList.remove("playing");
+      },
+      { once: true }
     );
-    }
+  }
 }
 
-window.addEventListener('keydown', (e) => {
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    
-    if(key) {
-        key.click();
-    }
+window.addEventListener("keydown", (e) => {
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (key) {
+    key.click();
+  }
 });
 
-
-
-keys.forEach(key => key.addEventListener('click', keyClicked));
+keys.forEach((key) => key.addEventListener("click", keyClicked));
